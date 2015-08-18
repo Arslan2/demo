fa281c3d5512c8939265ca8ea90e7cdf918901c6
Arslan2 <arslan.rao94@square63.com>
2015-08-18 18:50:13 +0500

    Product: Added functionality to upload attachments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ActiveRecord::Base
+
   PER_PAGE_SIZE = 5
-  attr_accessible :body, :price, :title
+  attr_accessible :body, :price, :title, :attachments_attributes
+
+  has_many :attachments, as: :attachable
+
+  accepts_nested_attributes_for :attachments, allow_destroy: true
 
   scope :ordered, -> { order("created_at desc") }
+
 end
