e9834b47f7d165f65817f0d1c96dc075ff30d792
Arslan2 <arslan.rao94@square63.com>
2015-08-19 12:13:05 +0500

    Review: Scaffold generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Demo::Application.routes.draw do
+  resources :reviews
+
+
   resources :products
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150818101937) do
+ActiveRecord::Schema.define(:version => 20150819071033) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "attachable_id"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(:version => 20150818101937) do
     t.float    "price"
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
+  end
+
+  create_table "reviews", :force => true do |t|
+    t.text     "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
