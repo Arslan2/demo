09749239c40758827e2d197ea51316ed6fc9d9ea
Arslan2 <arslan.rao94@square63.com>
2015-08-25 10:00:38 +0500

    Product: Added dependent destroy on attachments

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   PER_PAGE_SIZE = 5
   attr_accessible :body, :price, :title, :attachments_attributes
 
-  has_many :attachments, as: :attachable
+  has_many :attachments, as: :attachable, dependent: :destroy
   has_many  :reviews
   belongs_to :user
 
