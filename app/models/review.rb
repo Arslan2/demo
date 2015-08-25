371992c65a145dc0c7a5f7bfdedf3db42dec1bd4
Arslan2 <arslan.rao94@square63.com>
2015-08-25 10:03:49 +0500

    Review: Added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ActiveRecord::Base
   attr_accessible :comment
   belongs_to :product
   belongs_to :user
+
+  validates :comment, presence: true, length: { minimum: 2, maximum: 150 }
+
   scope :ordered, -> { order('updated_at desc') }
 end
