7ba2eac4f962816746b332743bfafeb95758a17c
Arslan2 <arslan.rao94@square63.com>
2015-08-27 17:56:25 +0500

    User: Show page is made public

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:dashboard]
   def show
     @user = User.find(params[:id])
     @products = @user.products.ordered.page(params[:page]).per(User::PER_PAGE)
