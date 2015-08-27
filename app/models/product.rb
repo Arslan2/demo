cf221301073e5fed1d537d418febd61dd18e0fb3
Arslan2 <arslan.rao94@square63.com>
2015-08-27 17:54:30 +0500

    Product: Fixed association with order_product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
 
   has_many :attachments, as: :attachable, dependent: :destroy
   has_many  :reviews, dependent: :destroy
+  has_many :order_products, dependent: :destroy
   has_many :orders, through: :order_products
   belongs_to :user
 
