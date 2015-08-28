0499f566aba85aefbf243d9ba8e6b34b15225892
Arslan2 <arslan.rao94@square63.com>
2015-08-28 19:47:37 +0500

    Product: Fixed create new product from admin dashboard issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 
   PER_PAGE_SIZE = 5
-  attr_accessible :body, :price, :title, :attachments_attributes
+  attr_accessible :body, :price, :title, :attachments_attributes, :user_id
 
   has_many :attachments, as: :attachable, dependent: :destroy
   has_many  :reviews, dependent: :destroy
