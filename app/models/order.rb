f08fa301c6b4278008e3db8b905f76cd320da735
Arslan2 <arslan.rao94@square63.com>
2015-08-31 16:36:13 +0500

    Order: Shipping address column added

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ActiveRecord::Base
   has_many :order_products, dependent: :destroy
   belongs_to :user
 
-  attr_accessible :amount, :discount
+  attr_accessible :amount, :discount, :shipping_address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150831112305) do
+ActiveRecord::Schema.define(:version => 20150831113005) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -74,9 +74,10 @@ ActiveRecord::Schema.define(:version => 20150831112305) do
   create_table "orders", :force => true do |t|
     t.float    "amount"
     t.float    "discount"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.integer  "user_id"
+    t.text     "shipping_address"
   end
 
   create_table "products", :force => true do |t|
