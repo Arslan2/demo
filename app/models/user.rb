ef6451ed9b7055ab3a41bc926c043c7abcb47519
Arslan2 <arslan.rao94@square63.com>
2015-08-31 16:00:47 +0500

    User: Address column added

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :attachment_attributes
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :attachment_attributes, :address
 
   validates :first_name, :last_name, presence: true, length: { minimum: 3, maximum: 50 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150825102818) do
+ActiveRecord::Schema.define(:version => 20150831105521) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(:version => 20150825102818) do
     t.datetime "updated_at",                                           :null => false
     t.string   "first_name",             :limit => 30
     t.string   "last_name",              :limit => 30
+    t.text     "address"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
