f50cdc2f7a7fb607807d5097def15cf46e617db1
Arslan2 <arslan.rao94@square63.com>
2015-08-31 16:34:08 +0500

    User: Added association with orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :products, through: :order_products
+  has_many :order_products, dependent: :destroy
+  belongs_to :user
 
   attr_accessible :amount, :discount
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_one :attachment, as: :attachable
   has_many :products, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many :orders, dependent: :destroy
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
