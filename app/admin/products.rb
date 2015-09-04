2c8d70a4982a0784505faf25af3b5f61af9e06fb
Arslan2 <arslan.rao94@square63.com>
2015-09-04 11:42:43 +0500

    AdminProduct: Fixed new indentation

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
     column :title
     column :body
     column "Created At", :created_at
-    column :price, :sortable => :price do |product|
+    column :price, sortable: :price do |product|
       div :class => "price" do
         number_to_currency product.price
       end
